ee357214774d4bd0d2d4e22e06626e6715a7b4d7
EdilsonRogerio <eddyrogerioyuran@gmail.com>
2022-10-28 14:22:04 -0300

    updating calculadora

--- day 10/calculator/main.py
+++ day 10/calculator/main.py
@@ -18,24 +18,42 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
+# Square
+def square(n1):
+    return n1 ** 2
+# Exponent
+def exponent(n1, n2):
+    return n1 ** n2
+
+# Guarda as operações em um dicionário
 operations = {
     "+": add,
     "-": subtract,
     "*": multiply,
-    "/": divide
+    "/": divide,
+    "**": square,
+    "^": exponent
 }
 
+# Função que calcula a operação
 def calculator():
+    print(logo)
+    print("Bem vindo a Calculadora!")
+    print("Voce pode somar, subtrair, multiplicar e dividir")
     num1 = float(input("Digite o primeiro número: "))
     num2 = float(input("Digite o segundo número: "))
     
+    # Imprime as operações
     for symbol in operations:
         print(symbol)
+    
     should_continue = True
     
     while should_continue:
+        # Pergunta qual operação o usuário deseja fazer
         operation_symbol = input("Escolha uma operação: ")
 
+        # Calcula a operação
         calculation_function = operations[operation_symbol]
         answer = calculation_function(num1, num2)
         print(f"{num1} {operation_symbol} {num2} = {answer}")
